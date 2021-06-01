801ebe3e7cd4639a7a4e23e3ca2b83a1a3b14702
김태주 <kim9xd@gmail.com>
2021-06-01 20:34:40 +0900

    git init

---
+++ src/test/java/me/pulpury/inflearnthejavatest/InflearnTheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pulpury.inflearnthejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnTheJavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
