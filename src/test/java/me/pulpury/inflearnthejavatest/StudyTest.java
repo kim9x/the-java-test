51518ec44f75dc794a027aaef6fcc6f4b0a22549
김태주 <kim9xd@gmail.com>
2021-06-04 18:00:57 +0900

    JUnit 5 조건에 따라 테스트 실행하기

--- src/test/java/me/pulpury/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/pulpury/inflearnthejavatest/StudyTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -21,55 +23,39 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
 	@Test
 	@DisplayName("스터디 만들기😭")
+	// 복수 선택 가능
+//	@EnabledOnOs(OS.MAC)
+	@EnabledOnOs({OS.MAC, OS.LINUX})
+//	@EnabledOnJre(JRE.JAVA_8)
+	@EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11})
+	@EnabledIfEnvironmentVariable(named = "TEST_EVN", matches = "LOCAL")
 	void create_new_study() {
-//		Study study = new Study(-10);
-//		assertNotNull(study);
-//		assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태 값이 " + StudyStatus.DRAFT + "여야 한다.");
+		// 시스템 변수를 eclipse를 킬 때 다 읽어오므로 변경 될 시
+		// eclipse 혹은 다른 IDE를 리부트 해줘야한다.
+		String test_env = System.getenv("TEST_ENV");
+		System.out.println(test_env);
+		assumeTrue("LOCAL".equalsIgnoreCase(test_env));
 		
-		// 위에처럼 넘기면 문자열 연상을 무조건(테스트 성공, 실패에 상관없이) 하게되는
-		// 아래처럼 넘기면 실패했을 때만 연산해서 부담감(성능의 입장에서)을 많이 줄여줄 수 있다. 
-//		assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태 값이 " + StudyStatus.DRAFT + "여야 한다.");
-		/* assertEquals(StudyStatus.ENDED, study.getStatus(), new Supplier<String>() {
-			@Override
-			public String get() {
-				System.out.println("스터디를 처음 만들면 상태 값이 DRAFT여야 한다.");
-				return "스터디를 처음 만들면 상태 값이 DRAFT여야 한다.";
-			}
-		}); */
-//		assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.");
+		assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+			// TODO Something
+			System.out.println("LOCAL");
+		});
 		
-		// 한번에 실행한 후에 실패가 된 것들을 알려준다.
-		/* assertAll(
-				() -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태 값이 " + StudyStatus.DRAFT + "여야 한다."),
-				() -> assertNotNull(study),
-				() -> assertTrue (study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-		); */
-		
-		// 에러 메세지도 받을 수 있다.
-		/* IllegalArgumentException exception = 
-				assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-		String message = exception.getMessage();
-		assertEquals("limit은 0보다 커야 한다.", message); */
-		
-		// 안에 들어있는 ex. Thread.sleep(300);가 끝날 때 까지 기다린다.
-		/* assertTimeout(Duration.ofMillis(100), () -> {
-			new Study(10);
-			Thread.sleep(300);
-		}); */
-		
-		// 100밀리초가 지나면 그냥 바로 끝난다.
-		// Thread Local 설정을 갖고 있는 코드가 있을 땐(ex. Transaction)
-		// assertTimeout를 쓰는게 낫다.
-		/* assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-			new Study(10);
-			Thread.sleep(300);
-		}); */
+		assumingThat("taeju".equalsIgnoreCase(test_env), () -> {
+			// TODO Something
+		});
 		
 		Study actual = new Study(10);
 		assertThat(actual.getLimit()).isGreaterThan(0);
@@ -79,11 +65,14 @@ class StudyTest {
 	// @Disabled는 해당 테스트를 비활성화 시킬 때 사용한다.
 	// ex) test가 깨질 때, 
 //	@Disabled
-//	void create1_new_study_again() {
-//		Study study = new Study();
-//		assertNotNull(study);
-//		System.out.println("create1");
-//	}
+	@DisabledOnOs(OS.MAC)
+	@EnabledOnJre(JRE.OTHER)
+	@EnabledIfEnvironmentVariable(named = "TEST_EVN", matches = "taeju")
+	void create1_new_study_again() {
+		Study study = new Study();
+		assertNotNull(study);
+		System.out.println("create1");
+	}
 	
 	// 모든 테스트 가 실행되기 전에 딱 1번 실행 된다.
 	// static과 default만 가능, private은 안됨.
